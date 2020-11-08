d243c69f8c840b9370aec3fb046eef8fdb48773a
Dmitriifd <idimka9@gmail.com>
2020-11-08 20:58:57 +0500

    added product -slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
+    
+    var mixer = mixitup('.product__inner-box');
+    
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+      });
+
+    $('.product-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
 });
